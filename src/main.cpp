4978b30a3133b91413e514ddc578883c187c1a81
Alex <vitiya@inbox.ru>
2024-01-18 01:18:24 +0300

    Added ResourceManager

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 
 #include "Renderer/ShaderProgram.h"
+#include "Resources/ResourceManager.h"
 
 GLfloat points[] = {
     0.2f, 0.5f, 0.0f,
@@ -15,24 +16,6 @@ GLfloat colors[] = {
     0.0f, 1.0f, 0.0f,
     0.0f, 0.0f, 1.0f
 };
-
-const char* vertex_shader = 
-"#version 330 core\n"
-"layout(location = 0) in vec3 vertex_position;"
-"layout(location = 1) in vec3 vertex_color;"
-"out vec3 color;"
-"void main() {"
-"   color = vertex_color;"
-"   gl_Position  = vec4(vertex_position, 1.0);"
-"}";
-
-const char* fragment_shader = 
-"#version 330 core\n"
-"in vec3 color;"
-"out vec4 frag_color;"
-"void main() {"
-"   frag_color = vec4(color, 1.0);"
-"}";
 
 
 int g_windowSizeX = 640;
@@ -55,9 +38,11 @@ void glfwKeyCallback(GLFWwindow* pWindow, int key, int scancode, int action, int
 }
 
 
-int main(void)
+int main(int argc, char** argv)
 {
 	{
+
+
 		// Initialize the library
         if (!glfwInit())
         {
@@ -121,70 +106,70 @@ int main(void)
 
             */
             //Заменили код выше объектным
-            std::string vertexShader(vertex_shader);
-            std::string fragmentShader(fragment_shader);
 
-            Renderer::ShaderProgram shaderProgram(vertexShader, fragmentShader);
 
-            if (!shaderProgram.isCompiled())
+
             {
-                std::cerr << "Can't create shader program!" << std::endl;
-                return -1;
-            }
+
+                ResourceManager resourceManager(argv[0]);
+
+                auto pDefaultShaderProgram = resourceManager.loadShaders("DefaultShader", "res/shaders/vertex.txt", "res/shaders/fragment.txt");
+                if (!pDefaultShaderProgram)
+                {
+                    std::cerr << "Can't create shader program: " << "DefaultShader" << std::endl;
+                    return -1;
+                }
 
 
+                // Генерируем буфер для информации от шейдеров
+                GLuint points_vbo = 0;
+                glGenBuffers(1, &points_vbo);
+                glBindBuffer(GL_ARRAY_BUFFER, points_vbo);
+                glBufferData(GL_ARRAY_BUFFER, sizeof(points), points, GL_STATIC_DRAW);
 
+                // Генерируем буфер для информации от шейдеров цвета
+                GLuint colors_vbo = 0;
+                glGenBuffers(1, &colors_vbo);
+                glBindBuffer(GL_ARRAY_BUFFER, colors_vbo);
+                glBufferData(GL_ARRAY_BUFFER, sizeof(colors), colors, GL_STATIC_DRAW);
 
+                //Сейчас пока карта не знает что делать с данной информацией буферов
 
-            // Генерируем буфер для информации от шейдеров
-            GLuint points_vbo = 0;
-            glGenBuffers(1, &points_vbo);
-            glBindBuffer(GL_ARRAY_BUFFER, points_vbo);
-            glBufferData(GL_ARRAY_BUFFER, sizeof(points), points, GL_STATIC_DRAW);
+                GLuint vao = 0;
+                glGenVertexArrays(1, &vao);
+                glBindVertexArray(vao);
 
-            // Генерируем буфер для информации от шейдеров цвета
-            GLuint colors_vbo = 0;
-            glGenBuffers(1, &colors_vbo);
-            glBindBuffer(GL_ARRAY_BUFFER, colors_vbo);
-            glBufferData(GL_ARRAY_BUFFER, sizeof(colors), colors, GL_STATIC_DRAW);
+                glEnableVertexAttribArray(0);
+                glBindBuffer(GL_ARRAY_BUFFER, points_vbo);
+                glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
 
-            //Сейчас пока карта не знает что делать с данной информацией буферов
+                glEnableVertexAttribArray(1);
+                glBindBuffer(GL_ARRAY_BUFFER, colors_vbo);
+                glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
 
-            GLuint vao = 0;
-            glGenVertexArrays(1, &vao);
-            glBindVertexArray(vao);
-
-            glEnableVertexAttribArray(0);
-            glBindBuffer(GL_ARRAY_BUFFER, points_vbo);
-            glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
-
-            glEnableVertexAttribArray(1);
-            glBindBuffer(GL_ARRAY_BUFFER, colors_vbo);
-            glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
-
-        /***************************************************************************/
+            /***************************************************************************/
        
-		// Loop until the user closes the window
-		while (!glfwWindowShouldClose(pWindow))
-		{
-			// Render here
-			glClear(GL_COLOR_BUFFER_BIT);
+		    // Loop until the user closes the window
+		    while (!glfwWindowShouldClose(pWindow))
+		    {
+			    // Render here
+			    glClear(GL_COLOR_BUFFER_BIT);
 
-            //glUseProgram(shader_program);
-            // вместо функции выше используем объетный метод
-            shaderProgram.use();
+                //glUseProgram(shader_program);
+                // вместо функции выше используем объетный метод
+                 pDefaultShaderProgram->use();
 
 
-            glBindVertexArray(vao);
-            glDrawArrays(GL_TRIANGLES, 0, 3);
+                glBindVertexArray(vao);
+                glDrawArrays(GL_TRIANGLES, 0, 3);
 
-			// Swap front and back buffers
-			glfwSwapBuffers(pWindow);
+			    // Swap front and back buffers
+			    glfwSwapBuffers(pWindow);
 
-			// Poll for and process events
-			glfwPollEvents();
-		}
-
+			    // Poll for and process events
+			    glfwPollEvents();
+		    }
+        }
 		glfwTerminate();
 	}
 	
