72e3276ae6fa677a33f51da83ab218994e10cdaf
Alex <vitiya@inbox.ru>
2024-01-18 12:19:56 +0300

    Added png textures import

--- src/main.cpp
+++ src/main.cpp
@@ -120,6 +120,7 @@ int main(int argc, char** argv)
                     return -1;
                 }
 
+                resourceManager.loadTexture("DefaultTexture", "res/textures/map_16x16.png");
 
                 // Генерируем буфер для информации от шейдеров
                 GLuint points_vbo = 0;
